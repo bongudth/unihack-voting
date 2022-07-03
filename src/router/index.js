da7793da7c86953aad727b8000a512de3d9c83a6
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-07-03 21:09:02 +0700

    implement the auth page

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vote from '../views/Vote.vue'
+import Auth from '../views/Auth.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Auth',
+    component: Auth
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'Vote',
+    component: Vote
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
---
+++ src/views/Auth.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="auth" class="h-screen flex flex-col items-center justify-center gap-16">
+    <img src="../assets/unihack_logo.png" class="h-24 object-contain" />
+    <button class="auth_button w-auto flex items-center justify-center gap-4 border-1 rounded-4xl p-2">
+      <img src="../assets/google-logo.png" class="h-10 object-contain" />
+      <span class="font-bold">Sign in with Google</span>
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Auth'
+}
+</script>
+
+<style scoped>
+#auth {
+  background-image: url("../assets/unihack_background.png");
+  background-size: cover;
+  background-position: center;
+}
+
+.auth_button {
+  background-color: transparent;
+  color: #fdfdfd;
+  border-color: #fdfdfd;
+  transition: all 0.3s ease-in-out;
+}
+
+.auth_button:hover {
+  background-color: #200343;
+}
+</style>
