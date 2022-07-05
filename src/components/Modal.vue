862d53494a9d3ffca75aa784be54ddc3b549e6ab
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-07-05 09:39:43 +0700

    responsive modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="flex flex-col 2xl:flex-row items-center gap-4 rounded-4xl p-8">
+    <div class="flex flex-col 2xl:flex-row items-center gap-4 rounded-4xl md:p-8">
       <div class="background_dark flex flex-col items-center rounded-4xl p-2 md:p-4 gap-4 md:gap-8">
         <img
           :src="card.image"
@@ -12,7 +12,7 @@
         <div class="desc">
           {{ card.desc }}
         </div>
-        <button @click="confirmVote" class="button_vote h-15 w-max flex items-center gap-4 rounded-2xl object-cover px-8">
+        <button @click="confirmVote" class="button_vote h-12 md:h-15 w-max flex items-center gap-4 rounded-2xl object-cover px-8">
           <i class="fa-regular fa-heart"></i>
           <span>Up vote</span>
         </button>
