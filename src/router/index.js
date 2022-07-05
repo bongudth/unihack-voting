bd9e34e1b93b9dbe90659d64f562671c877d9d26
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-07-05 09:29:18 +0700

    un auth middleware

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vote from '../views/Vote.vue'
 import Auth from '../views/Auth.vue'
+import unAuth from '../middleware/unAuth'
 import auth from '../middleware/auth'
 
 Vue.use(VueRouter)
@@ -10,7 +11,10 @@ const routes = [
   {
     path: '/signin',
     name: 'Auth',
-    component: Auth
+    component: Auth,
+    meta: {
+      middleware: { unAuth }
+    }
   },
   {
     path: '/',
@@ -28,6 +32,12 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  if (to.meta?.middleware?.unAuth) {
+    to.meta.middleware.unAuth({ next, router })
+  } else {
+    next()
+  }
+
   if (to.meta?.middleware?.auth) {
     to.meta.middleware.auth({ next, router })
   } else {
